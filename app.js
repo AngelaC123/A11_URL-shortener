047f8f922e5a51a8ef7b053ca2bc4c6ed1f08627
AngelaC123 <angelarare@gmail.com>
2022-03-14 11:26:48 +0800

    Initiate project Set up express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const port = 3000
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('This is a url shortener')
+})
+
+app.listen(port, () => {
+  console.log(`Express server is now listening on http://localhost:${port}`)
+})
